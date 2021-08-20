30c97f259b74cf3e4565f9f06132679e3031ac0b
gitwujiaolong <742265483@qq.com>
2021-08-20 19:08:51 +0800

    洋葱模型

--- example/02koa/compose.js
+++ example/02koa/compose.js
@@ -1,17 +1,22 @@
-const add = (x, y) => x + y
-const square = z => z * z
-const fn = (x, y) => square(add(x, y))
-console.log(fn(1,2))
+const add = (x, y) => x + y;
+const square = (z) => z * z;
+const square2 = (z) => z * z;
+/* const fn = (x, y) => square(add(x, y));
+console.log(fn(1, 2)); */
 
-const compose = function(...[first,...other]){
-  return function(...args){
-    let ret = first(...args)
-    other.forEach(fn=>{
-      ret = fn(ret)
-    })
-    return ret
-  }
+const compose = function ([first, ...other]) {
+  return function (...args) {
+    let ret = first(...args);
+    other.forEach((fn) => {
+      ret = fn(ret);
+    });
+    return ret;
+  };
+};
+const fn2 = compose([add, square, square]);
+console.log(fn2(1, 2));
+
+/* function compose2(...funcs) {
+  return funcs.reduce((a, b) => (...args) => a(b(...args)));
 }
-const fn2 = compose(...[add,square])
-console.log(fn2(1,2))
-
+console.log(compose2(...[square, add])(1, 2)); */
